a9b843a33e5cca4c089e51c63655027a8237f90d
AnnaWijetunga <annawijetunga@gmail.com>
2020-01-10 19:59:38 -0500

    Migrate tables

---
+++ db/migrate/20200111000136_create_expenses.rb
@@ -0,0 +1,11 @@
+class CreateExpenses < ActiveRecord::Migration
+  def change
+    create_table :expenses do |t|
+      t.string :vendor
+      t.string :description
+      t.date :date
+      t.decimal :total, precision: 10, scale: 2
+      t.integer :user_id
+    end
+  end
+end
