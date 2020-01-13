37b82e7f44b199ad5877780f5ab279a63ac6995b
AnnaWijetunga <annawijetunga@gmail.com>
2020-01-13 14:20:29 -0500

    Adds instances methods for total amount and sorting by date

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :expenses
+
+  def total_amount
+    self.expenses.collect {|expense| expense.total}.sum
+  end
+
+  def expenses_sort_by_date
+    self.expenses.sort_by {|expense| expense[:date]}.reverse
+  end
+
 end
