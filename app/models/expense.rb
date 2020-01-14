51e49a7c2d7b4edfce890cba7c82eec4988ee521
AnnaWijetunga <annawijetunga@gmail.com>
2020-01-14 18:42:06 -0500

    Update file names

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,13 +1,4 @@
-class User < ActiveRecord::Base
-  has_secure_password
-  has_many :expenses
-
-  def total_amount
-    self.expenses.collect {|expense| expense.total}.sum
-  end
-
-  def expenses_sort_by_date
-    self.expenses.sort_by {|expense| expense[:date]}.reverse
-  end
-
+class Expense < ActiveRecord::Base
+  belongs_to :user
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
-class Expense < ActiveRecord::Base
-  belongs_to :user
+class User < ActiveRecord::Base
+  has_secure_password
+  has_many :expenses
+
+  def total_amount
+    self.expenses.collect {|expense| expense.total}.sum
+  end
+
+  def expenses_sort_by_date
+    self.expenses.sort_by {|expense| expense[:date]}.reverse
+  end
+
 end
