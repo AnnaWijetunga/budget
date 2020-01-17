90d215e57065e1aa78edd23e7d6b27536f341df5
AnnaWijetunga <annawijetunga@gmail.com>
2020-01-17 09:16:32 -0500

    Update helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,4 +32,15 @@ class ApplicationController < Sinatra::Base
 
   end
 
+  # helpers do
+
+  #   def logged_in?
+  #     !!current_user
+  #   end
+
+  #   def current_user
+  #     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  #   end
+
+  # end
 end
