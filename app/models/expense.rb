437b9299d518dcb9c232cdcf0a135609a6849da0
AnnaWijetunga <annawijetunga@gmail.com>
2020-01-20 14:12:49 -0500

    Add notes

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,14 @@
 class Expense < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :user 
 end
+
+# ActiveRecord association
+
+# Associations are a set of macro-like class methods 
+# for tying objects together through foreign keys
+
+# Foreign key tells tables how to relate to each other
+
+# A belongs_to association sets up a one-to-one connection 
+# with another model, such that each instance of the declaring model 
+# "belongs to" one instance of the other model.
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password # ActiveRecord macro, works with bcrypt, gives us methods to authenticate a user/password 
-  has_many :expenses
-
+  has_many :expenses #ActiveRecord association - SQL command to read foreign keys, connects users with expenses
+  validates :email, uniqueness: true
+  
   def total_amount
     expenses.collect {|expense| expense.total}.sum
   end
@@ -11,3 +12,7 @@ class User < ActiveRecord::Base
   end
 
 end
+
+# A has_many association indicates a one-to-many connection with another model. 
+# This association indicates that each instance of the model has 
+# zero or more instances of another model.
